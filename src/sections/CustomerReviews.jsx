26adf26c4cd89ff7a38a190fb00fe7a4100b806b
Pawan Kumar <pawan4super30@gmail.com>
2023-09-12 14:23:29 +0530

    first commit

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { reviews } from '../constants';
+import ReviewCard from '../components/ReviewCard';
+
+const CustomerReviews = () => {
+  return (
+    <section className='max-container'>
+      <h3 className='font-bold font-palanquin 
+      text-4xl text-center'>
+        What Our{' '}
+        <span className='text-coral-red'>Customers </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 
+      max-w-lg text-center">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+
+      <div className='mt-24 flex flex-1 
+      justify-evenly items-center gap-14 
+      max-lg:flex-col'>
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} {...review} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default CustomerReviews
